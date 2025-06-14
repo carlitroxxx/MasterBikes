c19bbc428c68c12468965a33d90c90b4def48448
Mayckol2005 <may.mardones@duocuc.cl>
2025-06-14 15:31:15 -0400

    Avance

--- frontend/src/Front-Arriendo/arriendosApi.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8082/arriendos';
-
-export const solicitarArriendo = async (data) => {
-    return await axios.post(API_URL, data);
-};
